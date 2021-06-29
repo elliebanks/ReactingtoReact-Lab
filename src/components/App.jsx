4d3d4e13c5217026fca3fac0ffcc7542155c361f
Ellie Banks <elliebanks@ellies-mbp.attlocal.net>
2021-06-29 16:22:30 -0500

    updated

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import Greeter from './Greeter';
+
+useEffect(() => {
+    setTimeout(() => {
+        setLoaded(!loaded);
+    }, 3000);
+}, []);
+
+
+const App = () => {
+    const [username, setUsername] = useState('');
+    const [loaded, setLoaded] = useState(false);
+
+    if (loaded) {
+        return(
+            <>
+                <Greeter phrase="Hey there" name="Ellie" />
+                <Greeter phrase="My doggy is" name="Blu" />
+                <Greeter phrase="I love you" name="Britney Spears" />
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                <p>You are logging in as: {username} </p>
+            </>
+        );
+    } else {
+        return (
+            <>
+            <h1>Website Loading...</h1>
+            <button onClick={() => setLoaded(true)}>Load page!</button>
+            </>
+        );
+    };
+};      
+
+
+export default App;
